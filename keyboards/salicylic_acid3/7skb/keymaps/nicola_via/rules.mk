a5ba91814027b246c8076cfd92f368899676d361
Keiji Momose <keiji.momose@soliton.co.jp>
2025-02-12 10:08:21 +0900

    サリチル酸氏 下記キーボードのnicola_viaキーマップをqmk_userspace_viaへ移管した。
    - 7skb
    - ergoarrows
    - jisplit89
    
    非公式ながら、o51goのdefaultキーマップを追加。

--- keyboards/salicylic_acid3/7skb/keymaps/nicola_via/rules.mk
+++
@@ -1,6 +0,0 @@
-VIA_ENABLE = yes
-# Nicola
-LTO_ENABLE = yes
-SRC += nicola.c \
-       key_duration.c
-# Nicola
--- keyboards/salicylic_acid3/ergoarrows/keymaps/nicola_via/rules.mk
+++
@@ -1,4 +0,0 @@
-VIA_ENABLE = yes
-LTO_ENABLE = yes
-SRC += nicola.c \
-       key_duration.c
