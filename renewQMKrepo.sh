1c0fa548cfb2e47e9fd71c0a6bb6b3fe726fb61a
Keiji Momose <keiji.momose@soliton.co.jp>
2025-02-13 09:43:29 +0900

    renewQMKrepo.sh: to keep build environment, command line parameters added.

--- renewQMKrepo.sh
+++ renewQMKrepo.sh
@@ -1,12 +1,42 @@
 #! /bin/bash
 
-#Update for QMK MSYS/MSYS
-pacman -Syu
+if [ "$#" -ne 1 ]; then
+    echo "usage: ./renewQMKrepo.sh -mcg"
+    echo "-m -> Update MSYS(for Windows Build Environment)"
+    echo "-c -> Update QMK CLI"
+    echo "-g -> Update QMK Git Repositry"
 
-#Update for QMK CLI
-pip install -U -r requirements.txt
+    exit 1
+fi
 
-#Update for QMK Repo
-git fetch --all
-git merge upstream/master
-make git-submodule
+UPD_MSYS=FALSE
+UPD_CLI=FALSE
+UPD_GIT=FALSE
+
+OPT=
+OPTIND=
+while getopts cgm OPT ; do
+    case $OPT in
+        c) UPD_CLI=TRUE; ;;
+        g) UPD_GIT=TRUE; ;;
+        m) UPD_MSYS=TRUE; ;;
+    esac
+done
+shift $(expr $OPTIND - 1)
+
+if [ $UPD_MSYS = "TRUE" ] ; then
+    #Update for QMK MSYS/MSYS
+    pacman -Syu
+fi
+
+if [ $UPD_CLI = "TRUE" ] ; then
+    #Update for QMK CLI
+    pip install -U -r requirements.txt
+fi
+
+if [ $UPD_GIT = "TRUE" ] ; then
+    #Update for QMK Repo
+    git fetch --all
+    git merge upstream/master
+    make git-submodule
+fi
