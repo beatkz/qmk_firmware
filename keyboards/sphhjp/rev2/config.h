6735fe487878c6aa3d0f578494eae2b972f02194
Keiji Momose <keiji.momose@soliton.co.jp>
2025-09-18 16:11:59 +0900

    sphhjp wip: update to QMK 0.30.x

--- keyboards/sphhjp/rev2/config.h
+++ keyboards/sphhjp/rev2/config.h
@@ -5,18 +5,6 @@
 #define MATRIX_ROWS 10 
 #define MATRIX_COLS 8
 
-#define MATRIX_ROW_PINS       { B0, B1, B2, B3, B4 }
-#define MATRIX_COL_PINS       { B5, B6, C6, C7, D4, D5, D6, D7 }
-#define MATRIX_ROW_PINS_RIGHT { B0, B1, B2, B3, B4 }
-#define MATRIX_COL_PINS_RIGHT { B5, B6, C6, C7, D4, D5, D6, D7 }
-
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
-
-/* Split Keyboard specific options */
-#define SPLIT_HAND_PIN E6  /*fix pin. HIGH is left, LOW is right */
-#define SOFT_SERIAL_PIN D3 /* serial communication pin */
-
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
