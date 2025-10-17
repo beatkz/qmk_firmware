e238a6c3419798b32dbd863a59d3d2a35c8ecf0b
Keiji Momose <keiji.momose@soliton.co.jp>
2025-10-17 16:57:33 +0900

    pacmanのアップデートの確認をバイパスした。

--- renewQMKrepo.sh
+++ renewQMKrepo.sh
@@ -40,7 +40,7 @@ fi
 
 if [ $UPD_MSYS = "TRUE" ] ; then
     #Update for QMK MSYS/MSYS
-    pacman -Syu
+    pacman -Syu --noconfirm
 fi
 
 if [ $UPD_DNF = "TRUE" ] ; then
@@ -50,7 +50,7 @@ fi
 
 if [ $UPD_CLI = "TRUE" ] ; then
     #Update for QMK CLI
-    pip install -U -r requirements.txt
+    python3 -m pip install -U -r requirements.txt
 fi
 
 if [ $UPD_GIT = "TRUE" ] ; then
