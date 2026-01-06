fdbac7274d98ae5000f4774749f9b4fc5ad510e1
Keiji Momose <keiji.momose@soliton.co.jp>
2026-01-06 14:22:10 +0900

    renewQMKrepo.sh: removed captive interface on g,v option.

--- renewQMKrepo.sh
+++ renewQMKrepo.sh
@@ -56,12 +56,12 @@ fi
 if [ $UPD_GIT = "TRUE" ] ; then
     #Update for QMK Repo
     git fetch --all
-    git merge upstream/master
+    git merge --no-edit upstream/master
     make git-submodule
 fi
 
 if [ $UPD_US_VIA = "TRUE" ] ; then
     #Update for VIA's Userspace QMK Git Repo
     git fetch --all
-    git merge upstream/main
+    git merge --no-edit upstream/main
 fi
