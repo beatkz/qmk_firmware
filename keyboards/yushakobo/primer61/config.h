d0481b6a5facdc9829fb45bb53124cfe6f879a47
Keiji Momose <keiji.momose@gmail.com>
2026-08-18 16:50:13 +0900

    yushakobo/primer61: update for QMK 0.32.x or newer

---
+++ keyboards/yushakobo/primer61/config.h
@@ -0,0 +1,13 @@
+// Copyright 2023 yushakobo (@yushakobo)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* key matrix size */
+#define MATRIX_COLS 7
+#define MATRIX_ROWS 10
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
